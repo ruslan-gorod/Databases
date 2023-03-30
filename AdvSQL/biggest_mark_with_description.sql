7f3860d344936d270decd8a3aedfa2b161713f63
Ruslan_Malyavskyy <ruslan-gorod@ukr.net>
2023-03-30 23:36:40 +0300

    Changed query in biggest_mark_with_description.sql: deleted second COALESCE

--- AdvSQL/biggest_mark_with_description.sql
+++ AdvSQL/biggest_mark_with_description.sql
@@ -2,16 +2,13 @@
 
 SELECT s.name,
   COALESCE(s.surname, 'NoSurname') as surname,
-  COALESCE(
     CASE
       WHEN MAX(er.mark) >= 9 THEN 'EXCELLENT'
       WHEN MAX(er.mark) >= 7 THEN 'GOOD'
       WHEN MAX(er.mark) >= 4 THEN 'AVERAGE'
       WHEN MAX(er.mark) >= 1 THEN 'BAD'
       ELSE 'not passed'
-    END,
-    'not passed'
-  ) as biggest_mark_description
+	END as biggest_mark_description
 FROM student s
   LEFT JOIN exam_result er ON s.id = er.student_id
 GROUP BY s.id;
